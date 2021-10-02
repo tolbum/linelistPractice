3257a05b0e6c431a5b242a3f668662d388ef14bb
Tolulope Abolade <taabolade@gmail.com>
2021-10-02 12:39:51 +0100

    First Commit

---
+++ linelist.R
@@ -0,0 +1,9 @@
+## Line List Practice Project
+
+# Loading packages
+pacman::p_load("rio", 
+               "janitor", 
+               "tidyverse", 
+               "here", 
+               "lubridate", 
+               "epikit")
